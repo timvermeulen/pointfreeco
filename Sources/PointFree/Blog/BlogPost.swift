7a9b78bd1392641abed5c1107e851ccd28091c02
Tim Vermeulen <tim@tims-macbook-pro.local>
2018-08-01 00:31:57 +0200

    Spacing

--- Sources/PointFree/Blog/BlogPost.swift
+++ Sources/PointFree/Blog/BlogPost.swift
@@ -4,7 +4,7 @@ import Foundation
 import Prelude
 
 public struct BlogPost {
-    public struct Id: Newtype, ExpressibleByIntegerLiteral, Comparable { public let rawValue: Int }
+  public struct Id: Newtype, ExpressibleByIntegerLiteral, Comparable { public let rawValue: Int }
 
   public private(set) var author: Author?
   public private(set) var blurb: String
